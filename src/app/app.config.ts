d2f7a98f7b0fa08583e17f88726d278164f037f3
Marco Ornelas <kitosornelas@gmail.com>
2026-04-17 20:31:02 -0600

    fix: add page transitions with view transitions API

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes, withViewTransitions()),
+    provideClientHydration(),
+    provideAnimations()
+  ]
+};
